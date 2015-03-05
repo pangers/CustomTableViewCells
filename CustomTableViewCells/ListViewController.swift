12a38521a3559f309bca69882fddf633a33d8ed7
James Pang <j.pang91@gmail.com>
2015-03-05 11:26:32 +1100

    starting project

---
+++ CustomTableViewCells/ListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ListViewController.swift
+//  CustomTableViewCells
+//
+//  Created by James Pang on 4/03/2015.
+//  Copyright (c) 2015 James Pang. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
